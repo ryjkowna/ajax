a2ae55baf02e4032e5df1b6303beadba2decab90
ryjkowna <ryjkowna@gmail.com>
2019-01-17 20:55:22 +0100

    dodanie diva

--- js/main.js
+++ js/main.js
@@ -15,6 +15,7 @@
 $(function () {
     $('#przycisk').click(function () {
         $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (data) {
+            $("div").append
             $('#dane-programisty').html('<p> Imię: ' + data.imie + '</p>');
             $('#dane-programisty').append('<p> Nazwisko: ' + data.nazwisko + '</p>');
             $('#dane-programisty').append('<p> Zawód: ' + data.zawod + '</p>');
@@ -22,6 +23,9 @@ $(function () {
         });
     })
 })
+let $newdiv1 = $("<div id='dane-programisty'></div>");
+ 
+$("button").after($newdiv1);
 
 
 
